43b62ae05ef9ceb88b4af3c6f083a20e5a4953ce
Oliver <oclancy@gmail.com>
2015-06-08 21:45:17 +0100

    Add QuoteEngineApp

---
+++ QuoteEngineApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.FSharp.Core;
+
+namespace QuoteEngineApp
+{
+    class Program
+    {
+        static byte[] Processor(byte[] inBytes)
+        {
+            return new byte[1];
+        }
+
+        static void Main(string[] args)
+        {
+
+            QuoteEngine.Interfaces.ISubscriber server = new RabbitMq.Subscriber();
+            server.Subscribe("quote",  FSharpFunc<byte[],byte[]>.FromConverter(Processor) );
+            
+        }
+    }
+}

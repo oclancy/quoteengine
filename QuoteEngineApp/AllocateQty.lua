a6f5321fe896c5fb39c21eb268070f54f1e051bb
Oliver <oclancy@gmail.com>
2015-06-09 21:59:23 +0100

    update

--- QuoteEngineApp/AllocateQty.lua
+++ QuoteEngineApp/AllocateQty.lua
@@ -1,7 +1,12 @@
-﻿var qty = KEYS[1];
+﻿var qty = VARGS[1];
 var allocateFrom = KEYS[2];
-var quoteRequestId = KEYS[3];
 var quoteId = KEYS[3];
+var price = KEYS[4];
 
-if( redis.hget(allocateFrom, "ALLOCATED") >= qty )
-	redis.hset
+if redis.hget(allocateFrom, "Allocated") >= qty then
+    redis.call("hincrby", allocateFrom, "Allocated", qty*-1)
+    redis.call("hmset", quoteId, "Qty", qty, "Price", price, "ExpTime", redis.call("time") )
+    return qty
+else
+    return 0
+end
